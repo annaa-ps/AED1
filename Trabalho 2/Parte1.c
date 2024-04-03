7c4e00f60b0176da30b2bcfc1e82858e8c7d24b8
Anna Karolyna <129415573+annaa-ps@users.noreply.github.com>
2024-04-03 09:45:11 -0300

    Update Parte1.c

--- Trabalho 2/Parte1.c
+++ Trabalho 2/Parte1.c
@@ -1,4 +1,5 @@
 //ESTRUTURA DA LISTA SIMPLESMENTE ENCADEADA PARA OS DEPARTAMENTOS
+//Estrutura do nó para os departamentos
 typedef struct no{
   int codigo;
   char  nomeDepartamento[50];
@@ -6,6 +7,7 @@ typedef struct no{
   struct no *proximo; 
 }NoD;
 
+//Estrutura da lista encadeada simples para os departamentos
 typedef struct {
   NoD *inicio; 
   int tam; 
@@ -13,6 +15,7 @@ typedef struct {
 
 
 //ESTRUTURA DA LISTA DUPLAMENTE ENCADEADA PARA OS DADOS DOS FUNCIONÁRIOS
+//Estrutura do nó para os funcionários
 typedef struct no {
   char nome[50];
   char cpf[12];
@@ -23,8 +26,20 @@ typedef struct no {
   struct no *anterior;
 } NoF;
 
+//Estrutura da lista duplamente encadeada com nó cabeça para os funcionários
 typedef struct {
   NoF *cabeca; //Nó cabeça que marca o início da lista
   int tam;
 } ListaDadosFuncionarios;
 
+//Função para criar ums lista de departamentos 
+void criarListaDepartamentos(ListaDepartamentos *lista){
+    lista->inicio = NULL;
+    lista->tam = 0;
+}
+
+//Função para criar uma lista de funcionários
+void criarListaFuncionarios(ListaDadosFuncionarios *lista){
+    lista->cabeca = NULL;
+    lista->tam = 0; 
+}
