0104381a8177aef6efcbf6ed7f374cea496e8f41
Anna Karolyna <129415573+annaa-ps@users.noreply.github.com>
2024-04-05 11:17:49 -0300

    Update Parte1.c

--- Trabalho 2/Parte1.c
+++ Trabalho 2/Parte1.c
@@ -1,3 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<string.h>
+
 //ESTRUTURA DA LISTA SIMPLESMENTE ENCADEADA PARA OS DEPARTAMENTOS
 //Estrutura do nó para os departamentos
 typedef struct no{
@@ -44,6 +48,48 @@ void criarListaFuncionarios(ListaDadosFuncionarios *lista){
     lista->tam = 0; 
 }
 
+//Função para validar CPF
+int validarCPF(char *cpf){
+    int i, j, soma=0, digito1, digito2;
+    char cpfAux[11];
+    
+    //Remover os pontos e o hífen e copiar para cpfAux
+    for(i=0, j=0; i<11; i++){
+        if(cpf[i] != '.' && cpf[i]!= '-'){
+            cpfAux[j] = cpf[i];
+            j++;
+        }
+    }
+    
+    //Calculo do primeiro digito
+    for(i=0; i<9; i++){
+        soma+= (cpfAux - '0')*(10 - i);
+    }
+    digito1 = 11 - (soma % 11);
+    if(digito1 >= 10){
+        digito1 = 0; 
+    }
+    
+    //Calculo do segundo digito 
+    soma = 0;
+    for(i=0; i<10; i++){
+        soma+=(cpfAux[i] - '0')*(11 - i);
+    }
+    digito2 = 11 - (soma % 11);
+    if(digito2 >= 10){
+        digito2 = 0;
+    }
+    
+    //Veririficar se os digitos calculados sao iguais aos digitos fornecidos
+    if((digito1 == cpfAux[9] - '0')&&(digito2 == cpfAux[10] - '0')){
+        return 1; //CPF Válido
+    }else{
+        return 0; //CPF Inválido
+    }
+    
+}
+
+
 //Função para inserir um departamento
 void inserirDepartamento(ListaDepartamentos *lista, int codigo, char nomeDepartamento[50], float percentualBonificacao){
     NoD *novoDepartamento = malloc(sizeof(NoD));
@@ -62,6 +108,8 @@ void inserirDepartamento(ListaDepartamentos *lista, int codigo, char nomeDeparta
 void inserirdFuncionario(ListaDadosFuncionarios *lista, char nome[50], char cpf[12], int idade, float salarioBruto, char departamento[15]){
     NoF *novoFuncionario = malloc(sizeof(NoF));
     if(novoFuncionario){
+        //Realiza a validação do CPF antes de inserir o funcionário
+        if(validarCPF(cpf)){
         strcpy(novoFuncionario->nome,nome);
         strcpy(novoFuncionario->cpf, cpf);
         novoFuncionario->idade = idade;
@@ -73,7 +121,11 @@ void inserirdFuncionario(ListaDadosFuncionarios *lista, char nome[50], char cpf[
             lista->cabeca->anterior = novoFuncionario;
         }
         lista->cabeca = novoFuncionario;
-        lista->tam++;
+        lista->tam++;   
+        }else{
+            printf("\nCPF Invalido!\n");
+            free(novoFuncionario);
+        }
     }else{
         printf("\nErro ao alocar memoria para o funcionario!\n");
     }
