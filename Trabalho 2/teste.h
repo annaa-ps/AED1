30cf78dd7b0b06e24254afc1a31606d386782b53
Anna Karolyna <129415573+annaa-ps@users.noreply.github.com>
2024-04-10 08:50:51 -0300

    Update teste.h

--- Trabalho 2/teste.h
+++ Trabalho 2/teste.h
@@ -3,6 +3,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include<string.h>
+#define ERRO -1
+#define SUCESSO 0
+#define N_ENCOTRADO 1
+#define N_P_REMOVER 2
+#define VAZIO 3
+
 
 // ESTRUTURA DA LISTA SIMPLESMENTE ENCADEADA PARA OS DEPARTAMENTOS
 // Estrutura do nó para os departamentos
@@ -36,15 +42,21 @@ typedef struct {
     Departamento *cabeca; // Nó cabeça que marca o início da lista
 } ListaDadosFuncionarios;
 
+//DEPARTAMENTO
 void criarListaDepartamentos(ListaDepartamentos *lista);
-void criarListaFuncionarios(ListaDadosFuncionarios *lista);
-int validarCPF(char *cpf);
 void inserirDepartamento(ListaDepartamentos *lista, int codigo, char nomeDepartamento[50], float percentualBonificacao);
+int removerDepartamento(ListaDepartamentos *lista, ListaDadosFuncionarios *listaFuncionarios, int codigo);
 Departamento *buscarDepartamento(ListaDepartamentos *lista, int codigo);
+
+//FUNCIONARIO
+void criarListaFuncionarios(ListaDadosFuncionarios *lista);
 void inserirFuncionario(ListaDadosFuncionarios *lista, ListaDepartamentos *listaDepartamentos, char nome[50], char cpf[12], int idade, float salarioBruto, int codigoDepartamento);
-void removerFuncionario(ListaDadosFuncionarios *lista, char cpf[12]);
-void removerDepartamento(ListaDepartamentos *listaD, int codigo);
-Funcionario *buscarFuncionario(ListaDadosFuncionarios *lista, char cpf[12]);
+int removerFuncionario(ListaDadosFuncionarios *lista,char *cpf);
+Funcionario* buscarFuncionario(ListaDadosFuncionarios *lista, char *cpf);
+
+
+//EXTRAS
+int validarCPF(char *cpf);
 float calcularSalarioLiquido(float salarioBruto, float percentualBonificacao);
 
 #endif // TESTE_H_INCLUDED
