3c12480aac9d71e5134917cc3e8afc93960d525e
Anna Karolyna <129415573+annaa-ps@users.noreply.github.com>
2024-04-10 10:44:31 -0300

    Update main.c

--- Trabalho 2/main.c
+++ Trabalho 2/main.c
@@ -13,26 +13,27 @@ int main() {
 
     do {
         printf("\n-------------------------MENU DE OPCOES-------------------------------\n");
-        printf("1 - Inserir departamento\n2 - Inserir funcionario\n3 - Remover funcionario\n4 - Remover departamento\n");
-        printf("5 - Buscar funcionario\n6 - Listar departamentos\n7 - Listar funcionarios de um departamento\n");
-        printf("8 - Listar todos os funcionarios\n9 - Calcular salario liquido\n10 - Sair\n");
+        printf("[1] - Inserir departamento\n[2] - Inserir funcionario\n[3] - Remover funcionario\n[4] - Remover departamento\n");
+        printf("[5] - Buscar funcionario\n[6] - Listar departamentos\n[7] - Listar funcionarios de um departamento\n");
+        printf("[8] - Listar todos os funcionarios\n[9] - Calcular salario liquido\n[10]- Sair\n");
         printf("----------------------------------------------------------------------\n");
         scanf("%d", &opcao);
         getchar(); // Limpar o buffer do teclado
 
         switch (opcao) {
             case 1:
-                printf("Digite o codigo do departamento: ");
+                printf("\nDigite o codigo do departamento: ");
                 scanf("%d", &codigoDepartamento);
                 getchar(); // Limpar o buffer do teclado
                 printf("Digite o nome do departamento: ");
                 fgets(nome, 50, stdin);
                 printf("Digite o percentual de bonificacao do departamento: ");
                 scanf("%f", &percentualBonificacao);
-                inserirDepartamento(&listaDepartamentos, codigoDepartamento, nome, percentualBonificacao);
+                inserirDepartamentoOrdenado(&listaDepartamentos, codigoDepartamento, nome, percentualBonificacao);
+
                 break;
             case 2:
-                printf("Digite o nome do funcionario: ");
+                printf("\nDigite o nome do funcionario: ");
                 fgets(nome, 50, stdin);
                 printf("Digite o CPF do funcionario: ");
                 fgets(cpf, 12, stdin);
@@ -45,17 +46,32 @@ int main() {
                 inserirFuncionario(&listaFuncionarios, &listaDepartamentos, nome, cpf, idade, salarioBruto, codigoDepartamento);
                 break;
             case 3:
-                printf("Digite o CPF do funcionario para remover: ");
+                printf("\nDigite o CPF do funcionario para remover: "  );
                 fgets(cpf, 12, stdin);
-                removerFuncionario(&listaFuncionarios, cpf);
+                Funcionario *funcionarioRemovido = removerFuncionario(&listaFuncionarios, cpf);
+                if (funcionarioRemovido != NULL) {
+                    printf("\nFuncionario removido com sucesso.\n");
+                } else {
+                    printf("\nFuncionario nao encontrado ou lista vazia.\n");
+                }
                 break;
+
+
             case 4:
-                printf("Digite o codigo do departamento para remover: ");
+                printf("\nDigite o codigo do departamento para remover: ");
                 scanf("%d", &codigoDepartamento);
-               removerDepartamento(&listaDepartamentos, &listaFuncionarios, codigoDepartamento);
+                int resultadoRemocaoDep = removerDepartamento(&listaDepartamentos, &listaFuncionarios, codigoDepartamento);
+                if (resultadoRemocaoDep == SUCESSO) {
+                    printf("Departamento removido com sucesso.\n");
+                } else if (resultadoRemocaoDep == N_ENCOTRADO) {
+                    printf("Departamento nao encontrado.\n");
+                } else if (resultadoRemocaoDep == N_P_REMOVER) {
+                    printf("Remocao nao permitida, existem funcionarios associados a este departamento.\n");
+                }
                 break;
+
             case 5:
-                printf("Digite o CPF do funcionario para buscar: ");
+                printf("\nDigite o CPF do funcionario para buscar: ");
                 fgets(cpf, 12, stdin);
                 buscarFuncionario(&listaFuncionarios, cpf);
                 break;
@@ -78,9 +94,11 @@ int main() {
                     Departamento *departamento = buscarDepartamento(&listaDepartamentos, funcionario->codigoDepartamento);
                     if (departamento != NULL) {
                         float salarioLiquido = calcularSalarioLiquido(funcionario->salarioBruto, departamento->percentualBonificacao);
-                        printf("O salario liquido do funcionario e: %.2f\n", salarioLiquido);
+                        printf("-------------------------------------------------------------");
+                        printf("\nO salario liquido do funcionario e: %.2f\n", salarioLiquido);
+                        printf("-------------------------------------------------------------");
                     } else {
-                        printf("Departamento do funcionario nao encontrado.\n");
+                        printf("\nDepartamento do funcionario nao encontrado!\n");
                     }
                 } else {
                     printf("Funcionario nao encontrado.\n");
